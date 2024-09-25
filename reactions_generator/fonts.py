f464bde884eaf080413d8cf0a4ccdb9d46f7aaa2
Azat Ismagilov <azatismagilov00@gmail.com>
2024-09-25 21:04:50 +0500

    Update font loading algorithm
    1. Enhance error handling to provide better feedback if specified fonts are not found
    2. Implement cache invalidation when a new font is added
    3. Use try-except blocks for improved error handling
    4. Utilize Python's functools.cache decorator for improved performance
    5. Simplify function calls and improve readability

--- reactions_generator/fonts.py
+++ reactions_generator/fonts.py
@@ -4,23 +4,30 @@ from PIL import ImageFont
 
 
 @functools.cache
-def load_font(font: str, fallback: str) -> ImageFont.FreeTypeFont:
-    try:
-        return ImageFont.truetype(font)
-    except IOError:
-        return ImageFont.truetype(fallback)
+def load_font(fonts: tuple[str]) -> ImageFont.FreeTypeFont:
+    exception = Exception("No font found")
+    for font in fonts:
+        try:
+            return ImageFont.truetype(font)
+        except IOError as e:
+            exception = e
+    raise exception
 
 
 @functools.cache
 def load_helvetica(size: int) -> ImageFont.FreeTypeFont:
-    return load_font("Helvetica", "./helvetica.ttf").font_variant(size=size)
+    return load_font(("Helvetica", "helvetica.ttf", "./helvetica.ttf")).font_variant(
+        size=size
+    )
 
 
 @functools.cache
 def load_helvetica_bold(size: int) -> ImageFont.FreeTypeFont:
-    return load_font("Helvetica Bold", "./helvetica_bold.ttf").font_variant(size=size)
+    return load_font(
+        ("Helvetica Bold", "helvetica-bold.ttf", "./helvetica_bold.ttf")
+    ).font_variant(size=size)
 
 
 @functools.cache
 def load_monspaced(size: int) -> ImageFont.FreeTypeFont:
-    return load_font("Courier", "./courier.ttf").font_variant(size=size)
+    return load_font(("Courier", "cour.ttf", "./cour.ttf")).font_variant(size=size)
