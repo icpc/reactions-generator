ddc5d783709033e214187a5180fa4adeb8d16148
Azat Ismagilov <azatismagilov00@gmail.com>
2025-12-06 10:34:10 +0300

    run auto-format

--- reactions_generator/defaults.py
+++ reactions_generator/defaults.py
@@ -13,21 +13,21 @@ class Defaults:
     screen_source = "example/screen.mp4"
     background_source = "example/background_vertical.png"
     background_source_h = "example/background-moscow.png"
-#   default vertical story
-    background_position = (0,0,1080,1920)   
-    card_position = (40,807,1000,306)
-    webcam_position = (40,194.5,1000,562.5)
-    screen_position = (40,1163,1000,562.5)
-#   default horizontal story with webcam in bottom left and card in bottom right
-#    background_position_h = (0,0,1920,1080)
-#    card_position_h = (904,758,1000,306)
-#    webcam_position_h = (0,0,1920,1080)
-#    screen_position_h = (16,704,640,360)
-#   default horizontal story with webcam in top right left and card in top left
-    background_position_h = (0,0,1920,1080)
-    card_position_h = (16,16,1000,306)
-    webcam_position_h = (0,0,1920,1080)
-    screen_position_h = (1264,16,640,360)
+    #   default vertical story
+    background_position = (0, 0, 1080, 1920)
+    card_position = (40, 807, 1000, 306)
+    webcam_position = (40, 194.5, 1000, 562.5)
+    screen_position = (40, 1163, 1000, 562.5)
+    #   default horizontal story with webcam in bottom left and card in bottom right
+    #    background_position_h = (0,0,1920,1080)
+    #    card_position_h = (904,758,1000,306)
+    #    webcam_position_h = (0,0,1920,1080)
+    #    screen_position_h = (16,704,640,360)
+    #   default horizontal story with webcam in top right left and card in top left
+    background_position_h = (0, 0, 1920, 1080)
+    card_position_h = (16, 16, 1000, 306)
+    webcam_position_h = (0, 0, 1920, 1080)
+    screen_position_h = (1264, 16, 640, 360)
 
     success_audio_path = "example/success.mp3"
     fail_audio_path = "example/fail.mp3"
